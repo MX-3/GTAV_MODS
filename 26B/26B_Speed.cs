ad4586a0063746a3cff56baccfd4b0f57d06b140
Bogdans Krutilins <bkr1haya@gmail.com>
2017-05-28 23:17:36 +0100

    Create 26B_Speed.cs
    Initial release.

---
+++ 26B/26B_Speed.cs
@@ -0,0 +1,87 @@
+using GTA;
+using System;
+using System.Windows.Forms;
+
+
+public class Speed : Script
+
+{
+    Boolean carReady = false; // check if car is spawned.
+    Boolean bombReady = false; // check if bomb has been activated.
+    Ped player = Game.Player.Character; // reference to player's character model.
+    Vehicle theCar; // reference to boom car.
+
+    public Speed()
+    {
+        Tick += OnTick; // happens at every frame.
+        //KeyDown += OnKeyDown; // check when key is down.
+        KeyUp += OnKeyUp; // check when key is up.
+
+
+
+        Interval = 1000; // how many times tick event is run. 1000 = 1 sec.
+
+      
+
+    }
+
+    void OnTick(object sender, EventArgs e)
+    {
+        theCar = player.LastVehicle; // get name of car that the player is trying to enter.
+        
+
+        if(carReady == true) // check if car is spawned.
+        {
+            if (player.IsInVehicle(theCar) == true) // check if player is in the car.
+            {
+                
+                if(theCar.Speed > 30 && bombReady == false) // if speed above 30, activate *speed mode* .
+                {
+                    UI.ShowSubtitle("Bomb Armed.");
+                    bombReady = true; //activate bomb.
+                    
+                }
+
+                if(theCar.Speed < 28 && bombReady == true) // if speed below 28 and bomb is ready then kill player. (Below 28 due to an insta-explode bug).
+                {   
+                    theCar.Explode();
+                    bombReady = false; // deactivate bomb.
+                }
+
+            }
+        }
+
+    }
+
+    void OnKeyDown(object sender, KeyEventArgs e)
+    {
+
+    }
+
+    void OnKeyUp(object sender, KeyEventArgs e)
+    {
+       
+    
+        if(e.KeyCode == Keys.K) // if key pressed = k, spawn the "boom car".
+        {
+            
+
+            if(carReady == false)
+            {
+                UI.ShowSubtitle("Speed Mode Activated");
+                carReady = true;
+            }
+
+            else if (carReady == true)
+            {
+                UI.ShowSubtitle("Speed Mode Deactivated");
+                carReady = false;
+            }
+
+
+
+
+        }
+    }
+
+}
